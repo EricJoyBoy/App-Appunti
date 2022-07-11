4a97b1f2354d16dda3907290ea81f6feae64818f
Eric Marins <ericonelopes@gmail.com>
2022-07-11 14:24:27 +0200

    Modifiche generiche

--- app/src/main/java/it/uninsubria/appappunti/MainActivity.kt
+++ app/src/main/java/it/uninsubria/appappunti/MainActivity.kt
@@ -2,10 +2,29 @@ package it.uninsubria.appappunti
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import it.uninsubria.appappunti.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        // handle click on the button, login
+        binding.loginBtn.setOnClickListener {
+            // do something, login
+        }
+        // handle click on the button, register
+        binding.skipBtn.setOnClickListener {
+            // do something, register
+        }
+
+
+
     }
 }
